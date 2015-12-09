dd2fd3aecddfa8e82c71eeac2af2b19ed6a2cf83
Philipp Winter <phw@nymity.ch>
2015-12-09 11:06:05 -0500

    Consider router descriptors in neighbour search.
    Nearest-neighbour search is based on comparing the configuration of Tor
    relays.  The more data we have, the better.  This commit makes us
    consider router descriptors in addition to router statuses.

--- distance.go
+++ distance.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 
 	tor "git.torproject.org/user/phw/zoossh.git"
-	levenshtein "github.com/arbovm/levenshtein"
 	cluster "github.com/NullHypothesis/mlgo/cluster"
+	levenshtein "github.com/arbovm/levenshtein"
 	statistics "github.com/mcgrew/gostats"
 )
 
@@ -15,12 +15,11 @@ import (
 type Distance func(obj1, obj2 tor.Object) float32
 
 // Levenshtein determines the Levenshtein distance, a string metric, between
-// the two given objects.  Depending on whether we are dealing with router
-// statuses or descriptors, we generate different strings.  In contrast to
+// the given router statuses and descriptors.  In contrast to
 // LevenshteinVerbose, this function only returns the distance.
-func Levenshtein(obj1, obj2 tor.Object) float32 {
+func Levenshtein(stat1, stat2 *tor.RouterStatus, desc1, desc2 *tor.RouterDescriptor) float32 {
 
-	distance, _ := LevenshteinVerbose(obj1, obj2)
+	distance, _ := LevenshteinVerbose(stat1, stat2, desc1, desc2)
 	return distance
 }
 
@@ -36,60 +35,51 @@ func PearsonCorrelation(a, b []float64) float64 {
 }
 
 // LevenshteinVerbose determines the Levenshtein distance, a string metric,
-// between the two given Tor objects.  Depending on whether we are dealing with
-// router statuses or descriptors, we compare different strings.
-func LevenshteinVerbose(obj1, obj2 tor.Object) (float32, string) {
+// between the given router statuses and descriptors.
+func LevenshteinVerbose(status1, status2 *tor.RouterStatus, desc1, desc2 *tor.RouterDescriptor) (float32, string) {
 
 	var str1, str2 string
 
-	switch x := obj1.(type) {
-
-	// dir-spec.txt says that nicknames can have a maximum of 19 characters.
-
-	case *tor.RouterStatus:
-		status1 := x
-		status2 := obj2.(*tor.RouterStatus)
-
-		str1 = fmt.Sprintf("%19s%5d%5d%s%12s%20s%12d%s",
-			status1.Nickname,
-			status1.ORPort,
-			status1.DirPort,
-			status1.Publication,
-			RouterFlagsToString(&status1.Flags),
-			status1.TorVersion,
-			status1.Bandwidth,
-			status1.PortList)
-
-		str2 = fmt.Sprintf("%19s%5d%5d%s%12s%20s%12d%s",
-			status2.Nickname,
-			status2.ORPort,
-			status2.DirPort,
-			status2.Publication,
-			RouterFlagsToString(&status2.Flags),
-			status2.TorVersion,
-			status2.Bandwidth,
-			status2.PortList)
-
-	case *tor.RouterDescriptor:
-		desc1 := x
-		desc2 := obj2.(*tor.RouterDescriptor)
-
-		str1 = fmt.Sprintf("%19s%5d%5d%s",
-			desc1.Nickname,
-			desc1.ORPort,
-			desc1.DirPort,
-			desc1.Published)
-
-		str2 = fmt.Sprintf("%19s%5d%5d%s",
-			desc2.Nickname,
-			desc2.ORPort,
-			desc2.DirPort,
-			desc2.Published)
+	if desc1 == nil {
+		desc1 = new(tor.RouterDescriptor)
+	}
+	if desc2 == nil {
+		desc2 = new(tor.RouterDescriptor)
 	}
 
+	str1 = fmt.Sprintf("%s%s%d%d%s%s%s%d%d%s%s%d%s",
+		status1.Nickname,
+		status1.Address,
+		status1.ORPort,
+		status1.DirPort,
+		RouterFlagsToString(&status1.Flags),
+		status1.TorVersion,
+		status1.PortList,
+		desc1.BandwidthAvg,
+		desc1.BandwidthBurst,
+		desc1.OperatingSystem,
+		desc1.Published,
+		desc1.Uptime,
+		desc1.Contact)
+
+	str2 = fmt.Sprintf("%s%s%d%d%s%s%s%d%d%s%s%d%s",
+		status2.Nickname,
+		status2.Address,
+		status2.ORPort,
+		status2.DirPort,
+		RouterFlagsToString(&status2.Flags),
+		status2.TorVersion,
+		status2.PortList,
+		desc2.BandwidthAvg,
+		desc2.BandwidthBurst,
+		desc2.OperatingSystem,
+		desc2.Published,
+		desc2.Uptime,
+		desc2.Contact)
+
 	verbose := fmt.Sprintf("%s: %s\n%s: %s",
-		obj1.GetFingerprint()[:8], str1,
-		obj2.GetFingerprint()[:8], str2)
+		status1.Fingerprint[:8], str1,
+		status2.Fingerprint[:8], str2)
 
 	return float32(levenshtein.Distance(str1, str2)), verbose
 }
--- neighbour.go
+++ neighbour.go
@@ -11,6 +11,8 @@ import (
 	vptree "github.com/DataWraith/vptree"
 )
 
+type FingerprintMap map[tor.Fingerprint]bool
+
 // QuadraticComparison determines pairwise relay similarities for all relays in
 // the given object set.  If the given threshold is not 0, only relay pairs
 // whose distance fall under the threshold are printed to stdout.  distFunc is
@@ -49,13 +51,17 @@ func QuadraticComparison(objects tor.ObjectSet, distFunc Distance, threshold flo
 // VantagePointTreeSearch builds a vantage point tree out of the given objects.
 // It then attempts to find the nearest neighbours to the given relay
 // identified by its fingerprint.  The result is printed to stdout.
-func VantagePointTreeSearch(objects tor.ObjectSet, rootrelay tor.Fingerprint, neighbours int) {
+func VantagePointTreeSearch(objects tor.ObjectSet, params *CmdLineParams) (FingerprintMap, error) {
+
+	rootrelay := tor.Fingerprint(params.ReferenceRelay)
+	neighbours := params.Neighbours
+
+	foundNeighbours := make(FingerprintMap)
 
 	// Find the relay whose distance to all other relays is to be determined.
 	targetRelay, found := objects.GetObject(tor.SanitiseFingerprint(rootrelay))
 	if !found {
-		log.Fatalf("Could not find relay with fingerprint %s.", rootrelay)
-		return
+		return nil, fmt.Errorf("Could not find relay with fingerprint %s.", rootrelay)
 	}
 
 	// Convert object set to interface{} slice because that's what the
@@ -69,8 +75,12 @@ func VantagePointTreeSearch(objects tor.ObjectSet, rootrelay tor.Fingerprint, ne
 
 	// We need a wrapper for Levenshtein() because the levenshtein package's
 	// function signature differs from our Distance type.
-	lvnst := func(obj1, obj2 interface{}) float64 {
-		return float64(Levenshtein(obj1.(tor.Object), obj2.(tor.Object)))
+	lvnst := func(stat1, stat2 interface{}) float64 {
+		status1 := stat1.(*tor.RouterStatus)
+		status2 := stat2.(*tor.RouterStatus)
+		desc1, _ := tor.LoadDescriptorFromDigest(params.DescriptorDir, status1.Digest, status1.Publication)
+		desc2, _ := tor.LoadDescriptorFromDigest(params.DescriptorDir, status2.Digest, status2.Publication)
+		return float64(Levenshtein(status1, status2, desc1, desc2))
 	}
 
 	log.Println("Building vantage point tree.")
@@ -83,12 +93,17 @@ func VantagePointTreeSearch(objects tor.ObjectSet, rootrelay tor.Fingerprint, ne
 	similarRelays, distances := tree.Search(targetRelay, neighbours+1)
 	log.Printf("Found relays after looking for %s.", time.Since(now))
 
+	targetStatus := targetRelay.(*tor.RouterStatus)
+	targetDesc, _ := tor.LoadDescriptorFromDigest(params.DescriptorDir, targetStatus.Digest, targetStatus.Publication)
 	// We skip the most similar relay because it's targetRelay.
 	for i := 1; i < len(similarRelays); i++ {
 
-		similarRelay := similarRelays[i].(tor.Object)
+		similarRelay := similarRelays[i].(*tor.RouterStatus)
+		similarDesc, _ := tor.LoadDescriptorFromDigest(params.DescriptorDir, similarRelay.Digest, similarRelay.Publication)
 
-		_, comparedBlurb := LevenshteinVerbose(similarRelay, targetRelay)
+		foundNeighbours[similarRelay.Fingerprint] = true
+
+		_, comparedBlurb := LevenshteinVerbose(similarRelay, targetStatus, similarDesc, targetDesc)
 		fmt.Println(comparedBlurb)
 
 		fmt.Printf("Dist(%s, %s) = %.0f, <https://atlas.torproject.org/#details/%s>\n\n",
@@ -97,6 +112,8 @@ func VantagePointTreeSearch(objects tor.ObjectSet, rootrelay tor.Fingerprint, ne
 			distances[i],
 			similarRelay.GetFingerprint())
 	}
+
+	return foundNeighbours, nil
 }
 
 // FindNearestNeighbours attempts to find the n nearest neighbours for the
@@ -106,6 +123,6 @@ func FindNearestNeighbours(channel chan tor.ObjectSet, params *CmdLineParams, gr
 	defer group.Done()
 
 	for objects := range channel {
-		VantagePointTreeSearch(objects, tor.Fingerprint(params.ReferenceRelay), params.Neighbours)
+		VantagePointTreeSearch(objects, params)
 	}
 }
